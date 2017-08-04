909606ce1c3444d6db6807a35fabcfbf02e1b11a
Vatafu Vladut <vladvatafu9@gmail.com>
2017-08-04 03:34:33 +0300

    Update LeT-ME_SEE-_Y0u.py
    Some features added

--- LeT-ME_SEE-_Y0u.py
+++ LeT-ME_SEE-_Y0u.py
@@ -2,31 +2,61 @@
 import optparse
 import socket
 import nmap
+import pydoc
+import geocoder
 nm = nmap.PortScanner()
 parser = optparse.OptionParser()
-parser.add_option("-p","--port",type="int",dest="port")
-parser.add_option("--h0","--host",type="string",dest="host")
-#parser.add_option("--scan",type="callback",callback=scan_port(host,port))
+parser.add_option("-p","--port",action="store",type="int",dest="port",help="port to be suplied")
+parser.add_option("--h0","--host",type="string",action="store",help="the ip for given DNS",dest="host")
+#parser.add_option("--scan",type="callback",callback=scan_port(host,port)))
+parser.add_option("--msg","--message",type="string",action="store",dest="message",help="payload to be send to the server")
+parser.add_option("--ip",type="string",action="store",dest="ip",help=" ip dns lookup")
+parser.add_option("--c","--chat",action="store_true",dest="connection",help="if you want to chat")
+
 (options,args) = parser.parse_args()
 host = options.host
 port = options.port
-'''
-def connect(tghost,tgport):
+message = options.message
+ip = options.ip
+chat = options.connection
+
+def connect(tghost,tgport,payload_msg):
 	fd = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 	try:
 		fd.connect((tghost,tgport))
+		fd.send(payload_msg)
 		print "connected to port{}".format(port)
 		fd.close()
 
 	except socket.error:
 		print "cant connect to host,port closed{}".format(port)
-'''
 
-def scan_port(host):
+
+def find_open_port(host):
+	"""The name of this function is pretty explicit
+
+	   	with what is does.Also go check the documentation out.
+
+		It's free id doesn't cost anything.
+		https://pypi.python.org/pypi/python-nmap
+	"""
 	name = socket.gethostbyaddr(host)[0]
-	scanned = nm.scan(host,'20-43')
+	ip_from_name = socket.gethostbyname(ip)
+	print ip_from_name
+	print "INFO ABOUT THE SPECIFIC REVERSE DNS LOOKUP: {}".format(name)
+	scanned = nm.scan(host,'20-80')
 	all_hosts = nm.all_hosts()
-	print all_hosts
-	print nm[host]['tcp'].keys()
-#scan_port(host)
-#connect(host,port)
+	all_ports = nm[host].all_tcp()
+	print "ALL PORTS AVBILE FROM 20-80: {}".format(" ".join(str(x) for x in all_ports))
+	print "IS THERE ANY INFORMATION ABOUT PORT 80:{}".format(nm[host].has_tcp(80))
+	print "INFO FROM PORT 80 ABOUT THE HOST: {}".format(nm[host].tcp(80))
+	print "IS PORT 80 PORT OPEN:{}".format(nm[host]['tcp'][80]['state'])
+	g = geocoder.google(socket.gethostbyname(host)[0])
+	print "the lat and long for the server{}".format(g.latlng)
+
+
+
+
+#pydoc.doc(find_open_port)
+#find_open_port(host)
+#connect(host,port,message)
